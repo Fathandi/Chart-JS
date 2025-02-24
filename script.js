739b64ac831ffc68ae4d7cd399b99a56940001bf
Fathih Apriandi <168356153+Fathandi@users.noreply.github.com>
2025-02-24 23:02:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-        const data = {
-            labels: ['Hari 1', 'Hari 2', 'Hari 3', 'Hari 4', 'Hari 5', 'Hari 6', 'Hari 7'],
-            datasets: [
-                {
-                    label: 'Panjang Tanaman (cm)',
-                    data: [0, 1, 1.3, 1.8, 2.8, 3, 3],
-                    borderColor: '#2563eb',
-                    backgroundColor: 'rgba(37, 99, 235, 0.2)',
-                    yAxisID: 'y',
-                    tension: 0.4
-                },
-                {
-                    label: 'Jumlah Helai Daun',
-                    data: [0, 0, 2, 2, 2, 2, 3],
-                    borderColor: '#16a34a',
-                    backgroundColor: 'rgba(22, 163, 74, 0.2)',
-                    yAxisID: 'y1',
-                    tension: 0.4
-                }
-            ]
-        };
+const data = {
+  labels: ['Hari 1', 'Hari 2', 'Hari 3', 'Hari 4', 'Hari 5', 'Hari 6', 'Hari 7'],
+    datasets: [
+     {
+       label: 'Panjang Tanaman (cm)',
+       data: [0, 1, 1.3, 1.8, 2.8, 3, 3],
+       borderColor: '#2563eb',
+       backgroundColor: 'rgba(37, 99, 235, 0.2)',
+       yAxisID: 'y',
+       tension: 0.4
+     },
+     {
+       label: 'Jumlah Helai Daun',
+       data: [0, 0, 2, 2, 2, 2, 3],
+       borderColor: '#16a34a',
+       backgroundColor: 'rgba(22, 163, 74, 0.2)',
+       yAxisID: 'y1',
+       tension: 0.4
+     }
+   ]
+};
 
-        const config = {
-            type: 'line',
-            data: data,
+ 
+const config = {
+  type: 'line',
+    data: data,
             options: {
                 responsive: true,
                 interaction: {
